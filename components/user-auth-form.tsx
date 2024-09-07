71d5e450bd86e7bcf574dfcead9404b414fcdae0
Tsuyoshi Watanabe <tuyosi.628@gmail.com>
2024-09-07 16:08:18 +0900

    remove unnecessary codes

--- components/user-auth-form.tsx
+++ components/user-auth-form.tsx
@@ -3,11 +3,8 @@
 import * as React from "react";
 import { signIn } from "next-auth/react";
 
-import { cn } from "@/lib/utils";
-
 import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { Button, buttonVariants } from "./ui/button";
+import { Button } from "./ui/button";
 
 import {
   Select,
@@ -20,15 +17,7 @@ import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { toast } from "./ui/use-toast";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "./ui/form";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "./ui/form";
 
 const FormSchema = z.object({
   role: z.enum(["PARENT", "STAFF"]),
@@ -46,8 +35,6 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   });
 
   const onSubmit = async (data: z.infer<typeof FormSchema>) => {
-    console.log(data);
-
     const result = await signIn("credentials", {
       redirect: false,
       role: data.role,
